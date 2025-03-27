3a0555dd1aadbe45bcd90190de3e87e8b2eb991b
RichardPitoniak <richard106285@student-schoolsofwestfield.org>
2025-03-27 14:54:53 +0000

    minor formatting fixes

--- script.js
+++ script.js
@@ -12,41 +12,24 @@ let a = parseInt (prompt ("Enter your first number"))
 let b = parseInt (prompt ("Enter your second number"))
 let result = 0
 if (operation == "+") {result = add(a,b)}
-else if (operation == "-") { result = subtract(a,b)}
+else if (operation == "-") {result = subtract(a,b)}
 else if (operation == "*") {result = multiply(a,b)}
-else if (operation == "/") { result = divide(a,b)}
+else if (operation == "/") {result = divide(a,b)}
 alert (a + operation + b + " = " + result)
 }
-/* FUNCTION add(a,b)
- * define a function called add with two number parameters a and b
- * @param: a, b (integers)
- * @return: a + b
- */
+
 function add(a,b){
 return a + b
 }
 
-/* FUNCTION subtract(a,b)
- * define a function called subtract with two number parameters a and b
- * @param: a, b (integers)
- * @return: a - b
- */
 function subtract(a,b){
     return a - b
 }
-/* FUNCTION multiply(a,b)
- * define a function called multiply with two number parameters a and b
- * @param: a, b (integers)
- * @return: a * b (* means multiply)
- */
+
 function multiply(a,b){
     return a * b
 }
-/* FUNCTION divide(a,b)
- * define a function called divide with two number parameters a and b
- * @param: a, b (integers)
- * @return: a / b (/ means divided by)
- */
+
 function divide(a,b){
     return a / b
 }
